f8102f9e2afbe09e33dbaae7240ec0ebe27b8719
Jcbob <jakobwolski6@gmail.com>
2023-07-19 18:00:51 +0200

    created xcode files for design_practice project

---
+++ design_practice/design_practice/design_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  design_practiceApp.swift
+//  design_practice
+//
+//  Created by Jakub Wolski on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct design_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
