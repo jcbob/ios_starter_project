acba134d93d4585f976a72b279024e9ed326cdc4
Jcbob <jakobwolski6@gmail.com>
2023-08-19 15:50:36 +0200

    made first counting app|played around with code

--- design_practice/design_practice/ContentView.swift
+++ design_practice/design_practice/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
     var body: some View {
         VStack{
-            Spacer()
+            Text("\(count) Dracula")
+                .bold()
+                .font(.largeTitle)
+                .padding(20)
             HStack{
+                Button(action: {
+                    count += 1
+                }) {
+                    Text("Count Dracula")
+                        .padding(EdgeInsets(top:0, leading:55, bottom:0, trailing: 25))
+                }
+                
                 Spacer()
-                Text("buton")
-                    .padding()
-                    .background(Color.blue)
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                    .cornerRadius(15)
+                
+                Button(action: {
+                    count -= 1
+                }) {
+                    Text("Bye Dracula")
+                        .padding(EdgeInsets(top:0, leading:25, bottom:0, trailing: 55))
+                }
             }
         }
     }
