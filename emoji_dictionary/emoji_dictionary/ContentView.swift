213191e62ab3fc6f901fde99a42d5f925837348c
Jcbob <jakobwolski6@gmail.com>
2023-08-24 10:28:17 +0200

    created emoji_dictionary project

---
+++ emoji_dictionary/emoji_dictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  emoji_dictionary
+//
+//  Created by Jakub Wolski on 24/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ emoji_dictionary/emoji_dictionary/emoji_dictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_dictionaryApp.swift
+//  emoji_dictionary
+//
+//  Created by Jakub Wolski on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct emoji_dictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
